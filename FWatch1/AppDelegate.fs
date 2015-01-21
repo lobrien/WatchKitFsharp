9618eb92e76aa9008bd61010fe6b4c292fab2bad
lobrien <lobrien@knowing.net>
2015-01-21 12:37:56 -1000

    Initial commit
    Create a Unified App, plus two extensions. Switch the project-type GUIDs of the Watch App to {D73F8E79-B4DD-4AB0-A767-D9FA3E2FE740} (Watch App)

---
+++ FWatch1/AppDelegate.fs
@@ -0,0 +1,26 @@
+﻿namespace FWatch1
+
+open System
+
+open UIKit
+open Foundation
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit UIApplicationDelegate ()
+
+    override val Window = null with get, set
+
+    // This method is invoked when the application is ready to run.
+    override this.FinishedLaunching (app, options) =
+        this.Window <- new UIWindow (UIScreen.MainScreen.Bounds)
+        this.Window.RootViewController <- new FWatch1ViewController ()
+        this.Window.MakeKeyAndVisible ()
+        true
+
+module Main =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main (args, null, "AppDelegate")
+        0
+
---
+++ FWatch1/FWatch1ViewController.fs
@@ -0,0 +1,28 @@
+﻿namespace FWatch1
+
+open System
+open System.Drawing
+
+open Foundation
+open UIKit
+
+[<Register ("FWatch1ViewController")>]
+type FWatch1ViewController () =
+    inherit UIViewController ()
+
+    override x.DidReceiveMemoryWarning () =
+        // Releases the view if it doesn't have a superview.
+        base.DidReceiveMemoryWarning ()
+        // Release any cached data, images, etc that aren't in use.
+
+    override x.ViewDidLoad () =
+        base.ViewDidLoad ()
+        // Perform any additional setup after loading the view, typically from a nib.
+
+    override x.ShouldAutorotateToInterfaceOrientation (toInterfaceOrientation) =
+        // Return true for supported orientations
+        if UIDevice.CurrentDevice.UserInterfaceIdiom = UIUserInterfaceIdiom.Phone then
+           toInterfaceOrientation <> UIInterfaceOrientation.PortraitUpsideDown
+        else
+           true
+
